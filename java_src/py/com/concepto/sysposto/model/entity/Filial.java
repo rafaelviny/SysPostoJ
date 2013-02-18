94a68d1da4f942b9e6872c9a07c430a94b5004cc
Rafael <rafaeltoebe@gmail.com>
2013-02-17 23:44:17 -0300

    commitei

--- java_src/py/com/concepto/sysposto/model/entity/Filial.java
+++ java_src/py/com/concepto/sysposto/model/entity/Filial.java
@@ -1,14 +1,11 @@
 package py.com.concepto.sysposto.model.entity;
 
 import java.io.Serializable;
-import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
